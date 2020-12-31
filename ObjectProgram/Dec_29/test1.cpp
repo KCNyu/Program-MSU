5a2300d86e2331e0483719a63f3cd27ff66673ec
KCNyu <527580487@qq.com>
2020-12-31 17:13:55 +0800

    homework

--- ObjectProgram/Dec_29/test1.cpp
+++ ObjectProgram/Dec_29/test1.cpp
@@ -19,10 +19,44 @@ int g (list <int> & lst){
             if(p != lst.end()) p++;
         }
         return S;
-};
+}
+void p (list <int> & lst){
+        list<int> :: iterator p = lst.begin();
+        while (p != lst.end())
+        {
+            cout << "*p = " << *p << endl;
+            p++;
+        }
+}
+void f (const int x, const list <int> & lst){
+        list<int> :: const_iterator p = lst.begin();
+        while (p != lst.end())
+        {
+            if(*p == x){
+                cout << "YES!\n";
+                return;
+            }
+            p++;
+        }
+        cout << "NO!\n";
+}
+void h (list <int> & lst){
+    list<int> :: iterator p = lst.begin();
+    lst.insert(p,*p);
+    while (p != lst.end()){
+        p++;
+        if(p == lst.end()) break;
+        lst.insert(p,*p);
+    }
+    for(list<int>::reverse_iterator iter = lst.rbegin(); iter != lst.rend();){
+        cout << *(iter++) << endl;
+    }
+}
 int main(int argc, char *argv[])
 {
     list<int> l = {1,2,3,4,5,6};
     cout << g(l) << endl;
+    f(1,l);
+    h(l);
     return 0;
 }
