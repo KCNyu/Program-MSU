044ce5acd8b9c23df4b342a84c12eb288d0dccde
KCNyu <527580487@qq.com>
2021-05-20 17:12:12 +0800

    make dynamic library

---
+++ UnixProgram/Chapter3/inc/mymath.h
@@ -0,0 +1,7 @@
+#ifndef _MYMATH_H
+#define _MYMATH_H
+
+int add(int,int);
+int sub(int,int);
+
+#endif
---
+++ UnixProgram/Chapter3/test.cpp
@@ -0,0 +1,16 @@
+/*================================================================
+* Filename:test.cpp
+* Author: KCN_yu
+* Createtime:Thu 20 May 2021 01:46:22 AM CST
+================================================================*/
+
+#include <iostream>
+#include "mymath.h"
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    cout << add(1,2) << endl;
+    cout << sub(1,2) << endl;
+    return 0;
+}
