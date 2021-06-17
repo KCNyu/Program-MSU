d9aecfe8e2ef6d96fefe4481f4edba6f8be02a74
KCNyu <527580487@qq.com>
2021-06-17 13:36:24 +0800

    update

--- C++_ALGEBRA/TEST1/myTemp/matrix.cpp
+++ C++_ALGEBRA/TEST1/myTemp/matrix.cpp
@@ -4,14 +4,18 @@
  * Createtime:Wed 16 Jun 2021 08:31:20 PM CST
  ================================================================*/
 
+#include <chrono>
+#include <cmath>
 #include <cstdlib>
 #include <fstream>
 #include <iostream>
 #include <string>
 #include <vector>
-#include <cmath>
 
 using namespace std;
+using chrono::duration;
+using chrono::duration_cast;
+using chrono::high_resolution_clock;
 
 class Matrix_LU {
     private:
@@ -21,21 +25,20 @@ class Matrix_LU {
         vector<int> p_data;
         int row, col;
         const double eps = 0.00001;
+
     public:
         Matrix_LU(const char *filename);
         void PrintData();
         void WriteFile(string name);
         void WriteFile();
         void DecompLU();
-        double SumLrkUki(int r, int i);
-        double SumLikUkr(int r, int i);
 };
 Matrix_LU::Matrix_LU(const char *filename) {
 
     ifstream matrix(filename);
     if (!matrix.is_open()) {
         cerr << "open failed" << endl;
-        exit(0);
+        exit(1);
     }
 
     string temp;
@@ -63,7 +66,7 @@ Matrix_LU::Matrix_LU(const char *filename) {
     for (auto &r : data) {
         if (r.size() != row) {
             cerr << "row != col failed" << endl;
-            exit(0);
+            exit(1);
         }
     }
     col = data.back().size();
@@ -80,6 +83,7 @@ Matrix_LU::Matrix_LU(const char *filename) {
     matrix.close();
 }
 void Matrix_LU::PrintData() {
+    cout << "============A-MATRIX============" << endl;
     for (auto &row : data) {
         for (auto &elem : row) {
             cout.width(5);
@@ -103,135 +107,130 @@ void Matrix_LU::PrintData() {
         }
         cout << endl;
     }
+    cout << "============P-MATRIX============" << endl;
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < col; j++) {
+            cout.width(5);
+            cout << (j == p_data[i] ? 1 : 0) << " ";
+        }
+        cout << endl;
+    }
 }
 void Matrix_LU::WriteFile(string name) {
     ofstream mat;
     string prefix = name + " = ...\n  [";
     string suffix = ";\n   ";
     int n = row, i, j;
-    if(name == "L"){
-        mat.open("Lmat1_temp.m",ios::out);
+    if (name == "L") {
+        mat.open("Lmat1_temp.m", ios::out);
         mat << prefix;
-        for(i = 0; i < n; i++){
-            for(j = 0; j < n-1; j++){
+        for (i = 0; i < n; i++) {
+            for (j = 0; j < n - 1; j++) {
                 mat << l_data[i][j] << " ";
             }
             mat << l_data[i][j];
-            if(i < n-1){
+            if (i < n - 1) {
                 mat << suffix;
-            }
-            else{
+            } else {
                 mat << "];";
             }
         }
-    }
-    else if(name == "U"){
-        mat.open("Umat1_temp.m",ios::out);
+    } else if (name == "U") {
+        mat.open("Umat1_temp.m", ios::out);
         mat << prefix;
-        for(i = 0; i < n; i++){
-            for(j = 0; j < n-1; j++){
+        for (i = 0; i < n; i++) {
+            for (j = 0; j < n - 1; j++) {
                 mat << u_data[i][j] << " ";
             }
             mat << u_data[i][j];
-            if(i < n-1){
+            if (i < n - 1) {
                 mat << suffix;
+            } else {
+                mat << "];";
             }
-            else{
+        }
+    } else if (name == "P") {
+        mat.open("Pmat1_temp.m", ios::out);
+        mat << prefix;
+        for (i = 0; i < n; i++) {
+            for (j = 0; j < n - 1; j++) {
+                mat << (j == p_data[i] ? 1 : 0) << " ";
+            }
+            mat << (j == p_data[i] ? 1 : 0);
+            if (i < n - 1) {
+                mat << suffix;
+            } else {
                 mat << "];";
             }
         }
     }
 }
-void Matrix_LU::WriteFile(){
+void Matrix_LU::WriteFile() {
     WriteFile("L");
     WriteFile("U");
-}
-double Matrix_LU::SumLrkUki(int r, int i) {
-    double re = 0.0;
-    for (int k = 0; k < r; k++) {
-        re += l_data[r][k] * u_data[k][i];
-    }
-    return re;
-}
-double Matrix_LU::SumLikUkr(int r, int i) {
-    double re = 0.0;
-    for (int k = 0; k < r; k++) {
-        re += l_data[i][k] * u_data[k][r];
-    }
-    return re;
+    WriteFile("P");
 }
 void Matrix_LU::DecompLU() {
     if (row != col) {
         cerr << "Is not a square Matrix!" << endl;
-        exit(0);
+        exit(1);
     }
     int n = row;
-    for (int i = 0; i < n; i++){
+
+    for (int i = 0; i < n; i++) {
         l_data[i][i] = 1;
         p_data.push_back(i);
     }
-    for(int j = 0; j < n-1; j++){
-        double max_elem = fabs(data[j][j]);
-        int max_j = j;
-        for(int m = j+1; m < n; m++){
-            if(max_elem < fabs(data[m][j])){
-                max_elem = data[m][j];
-                max_j = m;
-            }
-        }
-        cout << "j = " << j << endl;
-        cout << "max_j = " << max_j << endl;
-        swap(data[j],data[max_j]);
-        swap(l_data[j],l_data[max_j]);
-        swap(u_data[j],u_data[max_j]);
-        swap(p_data[j],p_data[max_j]);
-        for(int i = j+1; i < n; i++){
-            if(fabs(data[j][j]) < eps){
-                cerr << "The matrix is not strict regular!" << endl;
-                exit(0);
-            }
-            l_data[i][j] = data[i][j]/data[j][j];
-            for(int k = j; k < n; k++){
-                data[i][k] -= l_data[i][j]*data[j][k];
-            }
-        }
-    }
-    for(int i = 0; i < n; i++){
-        for(int j = i; j < n; j++){
-            u_data[i][j] = data[i][j];
-        }
-    }
-    // l diag = 1;
+    // init L matrix diag and P matrix P[i] that's where 1 is
 
-    /*
-    for (int i = 0; i < n; i++) { // 第一步，计算L的第一列和U的第一行：U1i=A1i,Li1=Ai1/U11
-        u_data[0][i] = data[0][i];
-        l_data[i][0] = data[i][0] / u_data[0][0];
-    }
-    //计算U的第r行，L的第r列元素
-    // uri=ari-Σ(k=1->r-1)lrkuki    (i=r,r+1,...,n)
-    // lir=air-Σ(k=1->r-1)likukr    (i=r+1,r+2,...,n且r≠n)
-    for (int r = 1; r < n; r++) {
-        for (int i = r; i < n; i++) {
-            u_data[r][i] = data[r][i] - SumLrkUki(r, i);
-            if (i == r)
-                l_data[r][r] = 1;
-            else if (r == n)
-                l_data[n][n] = 1;
-            else{
-                if(fabs(u_data[r][r]) < eps){
-                    cerr << "The matrix is not strict regular!" << endl;
-                    exit(0);
-                }
-                l_data[i][r] = (data[i][r] - SumLikUkr(r, i)) / u_data[r][r];
+    u_data.assign(data.begin(), data.end());
+
+    // i -- the index of row
+    for (int i = 0; i < n - 1; i++) {
+        double max_elem = fabs(u_data[i][i]);
+        // find max elem for each man elem in diag
+        int max_i = i;
+        // save the max index
+        for (int j = i + 1; j < n; j++) {
+            if (max_elem < fabs(u_data[j][i])) {
+                max_elem = u_data[j][i];
+                max_i = j;
+            }
+        }
+        // find max in every row at lower triangle
+
+        swap(u_data[i], u_data[max_i]);
+        swap(p_data[i], p_data[max_i]);
+        // swap the row
+
+        for (int j = i + 1; j < n; j++) {
+            if (fabs(u_data[i][i]) < eps) {
+                cerr << "The matrix is not strict regular!" << endl;
+                exit(1);
+            }
+            l_data[j][i] = u_data[j][i] / u_data[i][i];
+            for (int k = i; k < n; k++) {
+                u_data[j][k] -= l_data[j][i] * u_data[i][k];
             }
         }
     }
-    */
 }
-int main() {
-    Matrix_LU t("Amat1.m");
-    t.DecompLU();
-    t.PrintData();
-    t.WriteFile();
+void PrintTime(high_resolution_clock::time_point start_time,
+        high_resolution_clock::time_point end_time) {
+    cout << "Time: "
+        << duration_cast<duration<double, milli>>(end_time - start_time).count()
+        << " ms" << endl;
+}
+void make_lu(const char *filename) {
+    const auto start_time = high_resolution_clock::now();
+    Matrix_LU m(filename);
+    m.DecompLU();
+    m.PrintData();
+    m.WriteFile();
+    const auto end_time = high_resolution_clock::now();
+    PrintTime(start_time, end_time);
+}
+int main()
+{
+    make_lu("Amat1.m");
 }
