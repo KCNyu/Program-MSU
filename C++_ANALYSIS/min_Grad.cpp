f80217fad67ce8a3823841a0fe1b94b674e4e909
KCNyu <527580487@qq.com>
2021-12-10 18:31:48 +0800

    update

--- C++_ANALYSIS/min_Grad.cpp
+++ C++_ANALYSIS/min_Grad.cpp
@@ -1,10 +1,11 @@
+#define GL_SILENCE_DEPRECATION
 #include <iostream>
 #include <stdlib.h>
 #include <cmath>
 #include <functional>
 #include <chrono>
+#include <GLUT/GLUT.h>
 #include <iomanip>
-#include <vector>
 
 using namespace std;
 using chrono::duration;
@@ -13,46 +14,163 @@ using chrono::high_resolution_clock;
 
 #define derta 0.000001
 
+double y1(double x, double c)
+{
+    return 0.1*(10*x-sqrt(10)*sqrt(c-x*x+2*x-1));
+}
+double y2(double x, double c)
+{
+    return 0.1*(10*x+sqrt(10)*sqrt(c-x*x+2*x-1));
+}
 double f(double x, double y)
 {
-	return 10 * pow(y - x * x, 2) + pow(1 - x, 2);
+    return 10 * pow(y - x * x, 2) + pow(1 - x, 2);
 }
 double derivatives_x(function<double(double, double)> const &f, double x, double y)
 {
-	return (f(x + derta, y) - f(x, y)) / derta;
+    return (f(x + derta, y) - f(x, y)) / derta;
 }
 double derivatives_y(function<double(double, double)> const &f, double x, double y)
 {
-	return (f(x, y + derta) - f(x, y)) / derta;
+    return (f(x, y + derta) - f(x, y)) / derta;
 }
+double falpha(double x, double y, double alpha)
+{
+    double dx = derivatives_x(f, x, y);
+    double dy = derivatives_y(f, x, y);
+    x -= alpha*dx;
+    y -= alpha*dy;
+    return f(x,y);
+}
+double gold(double fx(double, double, double alpha), double x, double y)
+{
+    double epsilon = 0.001, f_min,tau=0.618;
+    double a_gss= 0, b_gss = 1, f_lambda = 0, f_mu = 0;
+    double lambda = a_gss+(1.0- tau)*(b_gss-a_gss), mu = a_gss +tau*(b_gss - a_gss);
+    f_lambda= fx(x,y,lambda); f_mu = fx(x,y,mu);
+    for (int i = 0; fabs(mu- lambda) >epsilon; i++)
+    {
+        if (f_lambda < f_mu)
+        {
+            b_gss = mu; mu = lambda; f_mu = f_lambda;
+            lambda = a_gss + (1.0 - tau)*(b_gss - a_gss);
+            f_lambda = fx(x,y,lambda);
+        }
+        else
+        {
+            a_gss = lambda; lambda = mu; f_lambda = f_mu;
+            mu = a_gss + tau * (b_gss - a_gss);
+            f_mu = fx(x,y,mu);
+        }
+    }
+    f_min = fx(x,y,0.5*(lambda + mu));
+    
+    return (lambda+mu)/2;
+}
+
 double min_Grad(function<double(double, double)> const &f, double a, double b, double eps)
 {
-	double alpha = 0.01;
-	pair<double, double> x = {a, b};
-	pair<double, double> dx = {derivatives_x(f, x.first, x.second), derivatives_y(f, x.first, x.second)};
-
-	double f_old = f(x.first, x.second);
-
-	double f_result;
-	while (true)
-	{
-		x.first -= alpha * dx.first;
-		x.second -= alpha * dx.second;
-		f_result = f(x.first, x.second);
-		if (fabs(f_result - f_old) <= eps)
-		{
-			break;
-		}
-		dx.first = derivatives_x(f, x.first, x.second);
-		dx.second = derivatives_y(f, x.first, x.second);
-		f_old = f_result;
-	}
-	cout << fixed << setprecision(3) << "min_x = " << x.first << " min_y = " << x.second << endl;
-	cout << "f_result = " << f_result << endl;
-	return f_result;
+    double alpha;
+    pair<double, double> x = {a, b};
+    pair<double, double> dx = {derivatives_x(f, x.first, x.second), derivatives_y(f, x.first, x.second)};
+    
+    double f_old = f(x.first, x.second);
+    
+    double f_result;
+    while (true)
+    {
+        alpha = gold(falpha, x.first, x.second);
+        //cout << "alpha = " << alpha << endl;
+        x.first -= alpha * dx.first;
+        x.second -= alpha * dx.second;
+        f_result = f(x.first, x.second);
+        if (fabs(f_result - f_old) <= eps)
+        {
+            break;
+        }
+        dx.first = derivatives_x(f, x.first, x.second);
+        dx.second = derivatives_y(f, x.first, x.second);
+        f_old = f_result;
+    }
+    cout << fixed << setprecision(3) << "min_x = " << x.first << " min_y = " << x.second << endl;
+    cout << "f_result = " << f_result << endl;
+    return f_result;
 }
-int main(int argc, char const *argv[])
+void display()
+
 {
-	min_Grad(f, 0, 2, 0.00000001);
-	return 0;
+    
+    glClearColor(1,1,1,1);                     //设置刷新背景色
+    
+    glClear(GL_COLOR_BUFFER_BIT);              //刷新背景
+    
+    
+    
+    glBegin(GL_LINES);                //画个十字充当坐标系
+    
+    glColor3f(0,0,0);          //设置当前颜色
+    
+    glVertex3f(-1,0,0);
+    
+    glVertex3f(1,0,0);
+    
+    glVertex3f(0,-1,0);
+    
+    glVertex3f(0,1,0);
+    
+    glEnd();
+    
+    
+    for (float i = 0.1; i <= 10; i += 0.5)
+    {
+        glBegin(GL_LINE_STRIP);
+        glColor3f(1,0,0);
+        for (float x=-5;x<= 5;x+=0.001)
+            glVertex2f(x/5,y1(x,i)/5);
+        glEnd();
+        
+        glBegin(GL_LINE_STRIP);
+        glColor3f(1,0,0);
+        for (float x=-5;x<= 5;x+=0.001)
+            glVertex2f(x/5,y2(x,i)/5);
+        glEnd();
+    }
+    glFlush();          //更新窗口
+    
+}
+void PrintTime(high_resolution_clock::time_point start_time,
+               high_resolution_clock::time_point end_time)
+{
+    cout << "Time: "
+    << duration_cast<duration<double, milli>>(end_time - start_time).count()
+    << " ms" << endl;
+}
+
+int main(int argc, char *argv[])
+{
+    min_Grad(f, 0, 0, 0.00000001);
+    
+    glutInit(&argc, argv);
+    
+    //设置显示模式，RGB,单缓存
+    
+    glutInitDisplayMode(GLUT_RGB | GLUT_SINGLE);
+    
+    //设置窗体位置
+    
+    glutInitWindowPosition(1000, 1000);
+    
+    //设置窗体大小
+    
+    glutInitWindowSize(400, 400);
+    
+    //创建窗体但不显示
+    
+    glutCreateWindow("first");
+    
+    glutDisplayFunc(&display);
+    //显示
+    
+    glutMainLoop();
+    return 0;
 }
--- C++_ANALYSIS/min_Newton.cpp
+++ C++_ANALYSIS/min_Newton.cpp
@@ -1,8 +1,10 @@
+#define GL_SILENCE_DEPRECATION
 #include <iostream>
 #include <stdlib.h>
 #include <cmath>
 #include <functional>
 #include <chrono>
+#include <GLUT/GLUT.h>
 
 using namespace std;
 using chrono::duration;
@@ -13,46 +15,113 @@ using chrono::high_resolution_clock;
 
 double f(double x)
 {
-	return 2 * x - 1.0 + 2 * cos(M_PI * x);
+    return 2 * x - 1.0 + 2 * cos(M_PI * x);
 }
 double derivatives_1(function<double(double)> const &f, double x)
 {
-	return (f(x + derta) - f(x)) / derta;
+    return (f(x + derta) - f(x)) / derta;
 }
 double derivatives_2(function<double(double)> const &f, double x)
 {
-	return (derivatives_1(f, x + derta) - derivatives_1(f, x)) / derta;
+    return (derivatives_1(f, x + derta) - derivatives_1(f, x)) / derta;
 }
 double min_Newton(function<double(double)> const &f, double a, double b, double eps)
 {
-	double x1 = (a + b) / 2.0;
-	double x2 = x1 - derivatives_1(f, x1) / derivatives_2(f, x1);
-	while (fabs(x2 - x1) > eps)
-	{
-		x1 = x2;
-		x2 = x1 - derivatives_1(f, x1) / derivatives_2(f, x1);
-	}
-	return x2;
+    double x1 = (a + b) / 2.0;
+    double x2 = x1 - derivatives_1(f, x1) / derivatives_2(f, x1);
+    while (fabs(x2 - x1) > eps)
+    {
+        x1 = x2;
+        x2 = x1 - derivatives_1(f, x1) / derivatives_2(f, x1);
+    }
+    return x2;
+}
+void display()
+
+{
+    
+    glClearColor(1,1,1,1);                     //设置刷新背景色
+    
+    glClear(GL_COLOR_BUFFER_BIT);              //刷新背景
+    
+    
+    
+    glBegin(GL_LINES);                //画个十字充当坐标系
+    
+    glColor3f(0,0,0);          //设置当前颜色
+    
+    glVertex3f(-1,0,0);
+    
+    glVertex3f(1,0,0);
+    
+    glVertex3f(0,-1,0);
+    
+    glVertex3f(0,1,0);
+    
+    glEnd();
+    
+    
+    
+    glBegin(GL_LINE_STRIP);
+    
+    glColor3f(1,0,0);
+    for (float x=-5.00;x<= 5.00;x+=0.01)
+        glVertex2f(x/5,f(x)/10);
+    
+    glEnd();
+    
+    
+    glBegin(GL_LINE_STRIP);
+    
+    glColor3f(0,1,0);
+    glVertex2f(0.896884/5, 1);
+    glVertex2f(0.896884/5, -1);
+    glEnd();
+    glFlush();          //更新窗口
+    
 }
 void PrintTime(high_resolution_clock::time_point start_time,
-	       high_resolution_clock::time_point end_time)
+               high_resolution_clock::time_point end_time)
 {
-	cout << "Time: "
-	     << duration_cast<duration<double, milli>>(end_time - start_time).count()
-	     << " ms" << endl;
+    cout << "Time: "
+    << duration_cast<duration<double, milli>>(end_time - start_time).count()
+    << " ms" << endl;
 }
 void test(double eps)
 {
-	const auto start_time = high_resolution_clock::now();
-	double min_x = min_Newton(f, 0, 2, eps);
-	double min = f(min_x);
-	const auto end_time = high_resolution_clock::now();
-	cout << "min_x = " << min_x << endl;
-	cout << "min = " << min << endl;
-	PrintTime(start_time, end_time);
+    const auto start_time = high_resolution_clock::now();
+    double min_x = min_Newton(f, 0, 2, eps);
+    double min = f(min_x);
+    const auto end_time = high_resolution_clock::now();
+    cout << "min_x = " << min_x << endl;
+    cout << "min = " << min << endl;
+    PrintTime(start_time, end_time);
 }
-int main(int argc, char const *argv[])
+int main(int argc, char *argv[])
 {
-	test(0.000001);
-	return 0;
+    test(0.000001);
+    
+    glutInit(&argc, argv);
+    
+    //设置显示模式，RGB,单缓存
+    
+    glutInitDisplayMode(GLUT_RGB | GLUT_SINGLE);
+    
+    //设置窗体位置
+    
+    glutInitWindowPosition(1000, 1000);
+    
+    //设置窗体大小
+    
+    glutInitWindowSize(400, 400);
+    
+    //创建窗体但不显示
+    
+    glutCreateWindow("first");
+    
+    glutDisplayFunc(&display);
+    //显示
+    
+    glutMainLoop();
+    return 0;
 }
