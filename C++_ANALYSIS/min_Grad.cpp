95fa52480179a3e8321bcc5241e5a9f56f01f162
KCNyu <527580487@qq.com>
2021-12-15 23:35:05 +0800

    change Coordinate

--- C++_ANALYSIS/min_Grad.cpp
+++ C++_ANALYSIS/min_Grad.cpp
@@ -16,11 +16,11 @@ using chrono::high_resolution_clock;
 
 double y1(double x, double c)
 {
-    return 0.1*(10*x*x-sqrt(10)*sqrt(c-x*x+2*x-1));
+    return 0.1 * (10 * x * x - sqrt(10) * sqrt(c - x * x + 2 * x - 1));
 }
 double y2(double x, double c)
 {
-    return 0.1*(10*x*x+sqrt(10)*sqrt(c-x*x+2*x-1));
+    return 0.1 * (10 * x * x + sqrt(10) * sqrt(c - x * x + 2 * x - 1));
 }
 double f(double x, double y)
 {
@@ -38,34 +38,39 @@ double falpha(double x, double y, double alpha)
 {
     double dx = derivatives_x(f, x, y);
     double dy = derivatives_y(f, x, y);
-    x -= alpha*dx;
-    y -= alpha*dy;
-    return f(x,y);
+    x -= alpha * dx;
+    y -= alpha * dy;
+    return f(x, y);
 }
 double gold(double fx(double, double, double alpha), double x, double y)
 {
-    double epsilon = 0.001, f_min,tau=0.618;
-    double a_gss= 0, b_gss = 1, f_lambda = 0, f_mu = 0;
-    double lambda = a_gss+(1.0- tau)*(b_gss-a_gss), mu = a_gss +tau*(b_gss - a_gss);
-    f_lambda= fx(x,y,lambda); f_mu = fx(x,y,mu);
-    for (int i = 0; fabs(mu- lambda) >epsilon; i++)
+    double epsilon = 0.001, f_min, tau = 0.618;
+    double a_gss = 0, b_gss = 1, f_lambda = 0, f_mu = 0;
+    double lambda = a_gss + (1.0 - tau) * (b_gss - a_gss), mu = a_gss + tau * (b_gss - a_gss);
+    f_lambda = fx(x, y, lambda);
+    f_mu = fx(x, y, mu);
+    for (int i = 0; fabs(mu - lambda) > epsilon; i++)
     {
         if (f_lambda < f_mu)
         {
-            b_gss = mu; mu = lambda; f_mu = f_lambda;
-            lambda = a_gss + (1.0 - tau)*(b_gss - a_gss);
-            f_lambda = fx(x,y,lambda);
+            b_gss = mu;
+            mu = lambda;
+            f_mu = f_lambda;
+            lambda = a_gss + (1.0 - tau) * (b_gss - a_gss);
+            f_lambda = fx(x, y, lambda);
         }
         else
         {
-            a_gss = lambda; lambda = mu; f_lambda = f_mu;
+            a_gss = lambda;
+            lambda = mu;
+            f_lambda = f_mu;
             mu = a_gss + tau * (b_gss - a_gss);
-            f_mu = fx(x,y,mu);
+            f_mu = fx(x, y, mu);
         }
     }
-    f_min = fx(x,y,0.5*(lambda + mu));
-    
-    return (lambda+mu)/2;
+    f_min = fx(x, y, 0.5 * (lambda + mu));
+
+    return (lambda + mu) / 2;
 }
 
 double min_Grad(function<double(double, double)> const &f, double a, double b, double eps)
@@ -73,14 +78,14 @@ double min_Grad(function<double(double, double)> const &f, double a, double b, d
     double alpha;
     pair<double, double> x = {a, b};
     pair<double, double> dx = {derivatives_x(f, x.first, x.second), derivatives_y(f, x.first, x.second)};
-    
+
     double f_old = f(x.first, x.second);
-    
+
     double f_result;
     while (true)
     {
         alpha = gold(falpha, x.first, x.second);
-        //cout << "alpha = " << alpha << endl;
+        // cout << "alpha = " << alpha << endl;
         x.first -= alpha * dx.first;
         x.second -= alpha * dx.second;
         f_result = f(x.first, x.second);
@@ -99,78 +104,83 @@ double min_Grad(function<double(double, double)> const &f, double a, double b, d
 void display()
 
 {
-    
-    glClearColor(1,1,1,1);                     //设置刷新背景色
-    
-    glClear(GL_COLOR_BUFFER_BIT);              //刷新背景
-    
-    
-    
-    glBegin(GL_LINES);                //画个十字充当坐标系
-    
-    glColor3f(0,0,0);          //设置当前颜色
-    
-    glVertex3f(-1,0,0);
-    
-    glVertex3f(1,0,0);
-    
-    glVertex3f(0,-1,0);
-    
-    glVertex3f(0,1,0);
-    
+
+    glClearColor(1, 1, 1, 1); //设置刷新背景色
+
+    glClear(GL_COLOR_BUFFER_BIT); //刷新背景
+
+    glBegin(GL_LINES); //画个十字充当坐标系
+
+    glColor3f(0, 0, 0); //设置当前颜色
+
+    glVertex3f(-5, 0, 0);
+
+    glVertex3f(5, 0, 0);
+
+    glVertex3f(0, -5, 0);
+
+    glVertex3f(0, 5, 0);
+
     glEnd();
-    
-    
+
     for (float i = 0.1; i <= 10; i += 0.5)
     {
         glBegin(GL_LINE_STRIP);
-        glColor3f(1,0,0);
-        for (float x=-5;x<= 5;x+=0.001)
-            glVertex2f(x/5,y1(x,i)/5);
+        glColor3f(1, 0, 0);
+        for (float x = -5; x <= 5; x += 0.001)
+            glVertex2f(x, y1(x, i));
         glEnd();
-        
+
         glBegin(GL_LINE_STRIP);
-        glColor3f(1,0,0);
-        for (float x=-5;x<= 5;x+=0.001)
-            glVertex2f(x/5,y2(x,i)/5);
+        glColor3f(1, 0, 0);
+        for (float x = -5; x <= 5; x += 0.001)
+            glVertex2f(x, y2(x, i));
         glEnd();
     }
-    glFlush();          //更新窗口
-    
+    glFlush(); //更新窗口
 }
 void PrintTime(high_resolution_clock::time_point start_time,
                high_resolution_clock::time_point end_time)
 {
     cout << "Time: "
-    << duration_cast<duration<double, milli>>(end_time - start_time).count()
-    << " ms" << endl;
+         << duration_cast<duration<double, milli>>(end_time - start_time).count()
+         << " ms" << endl;
+}
+//转换坐标系
+void init(void)
+{
+    //设置显示窗口的背景为白色。参数顺序为：红、绿、蓝、透明度。
+    glClearColor(1.0, 1.0, 1.0, 0.0);
+    //设置投影类型：正投影
+    glMatrixMode(GL_PROJECTION | GL_MODELVIEW);
+    gluOrtho2D(-5, 5, -5, 5);
 }
 
 int main(int argc, char *argv[])
 {
     min_Grad(f, 0, 0, 0.00000001);
-    
+
     glutInit(&argc, argv);
-    
+
     //设置显示模式，RGB,单缓存
-    
+
     glutInitDisplayMode(GLUT_RGB | GLUT_SINGLE);
-    
+
     //设置窗体位置
-    
+
     glutInitWindowPosition(1000, 1000);
-    
+
     //设置窗体大小
-    
+
     glutInitWindowSize(400, 400);
-    
+
     //创建窗体但不显示
-    
+
     glutCreateWindow("first");
-    
+    init();
     glutDisplayFunc(&display);
     //显示
-    
+
     glutMainLoop();
     return 0;
 }
